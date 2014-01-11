5acc2706bf7437ce0c161ae47b68cd07d6bf2b70
James Gladney <jamezs@angelofcode.com>
2014-01-10 23:27:55 -0500

    Proper initiation up to Analysis function
    1. Buffer added to Global
    2. Having issues with arguments.
    3. New structures, but they dont seem to work or be needed at the
    moment.
    4. Deck upload format and loader.
    5. Process engine.

--- ygocore.js
+++ ygocore.js
@@ -1,5 +1,5 @@
 /* we are using JSLint to annoy us so here are some globals */
-/* globals require, process, console, __dirname */
+/* globals require, process, console, __dirname, Buffer */
 
 var prompt      = require('prompt'); /* debugging prompt, use in development */
 var WebSocket   = require('ws'); /* websockets */
@@ -11,20 +11,23 @@ var ffi         = require('ffi'); /* allows dynamic linking of the ocgapi.dll */
 var MersenneTwister    = require('mersennetwister'); /* shuffler*/
 
 //Constances
+console.log(process.argv);
+var launchpostion = 0;
+                    
 var configuration = {   
-    'version'   : (process.argv[(launchpostion+0)] ? process.argv[(launchpostion+0)] : 1),
-    'port'      : (process.argv[(launchpostion+1)] ? process.argv[(launchpostion+0)] : 1337),
-    'banlist'   : (process.argv[(launchpostion+2)] ? process.argv[(launchpostion+0)] : 0),
-    'cardpool'  : (process.argv[(launchpostion+3)] ? process.argv[(launchpostion+0)] : 2),
-    'gamemode'  : (process.argv[(launchpostion+4)] ? process.argv[(launchpostion+0)] : 2),
-    'priority'  : (process.argv[(launchpostion+5)] ? process.argv[(launchpostion+0)] : 0),
-    'deckcheck' : (process.argv[(launchpostion+6)] ? process.argv[(launchpostion+0)] : 0),
-    'shuffle'   : (process.argv[(launchpostion+7)] ? process.argv[(launchpostion+0)] : 0),
-    'startLP'   : (process.argv[(launchpostion+8)] ? process.argv[(launchpostion+0)] : 8000),
-    'starthand' : (process.argv[(launchpostion+9)] ? process.argv[(launchpostion+0)] : 5),
-    'draw'      : (process.argv[(launchpostion+10)] ? process.argv[(launchpostion+0)] : 1),
-    'timer'     : (process.argv[(launchpostion+11)] ? process.argv[(launchpostion+0)] : 180),
-    'production': (process.argv[(launchpostion+12)] ? process.argv[(launchpostion+0)] : 0)
+    'version'   : (1),
+    'port'      : (1337),
+    'banlist'   : (0),
+    'cardpool'  : ( 2),
+    'gamemode'  : ( 2),
+    'priority'  : (0),
+    'deckcheck' : ( 0),
+    'shuffle'   : ( 0),
+    'startLP'   : ( 8000),
+    'starthand' : ( 5),
+    'draw'      : (1),
+    'timer'     : ( 180),
+    'production': ( 0)
                     };
 var gamestate= {
     started     : false,
@@ -43,31 +46,43 @@ console.log('Configuration Loaded');
 var player1 = 0;
 var player2 = 1;
 
-var card_data = ref.refType('uint32');
-var pduel = ref.refType('uint32');
-var script_reader   = struct({
-    script_name : 'char*',
-    len         : 'int*'
+var card_data = struct({
+    code        : 'uint32',
+    alias       : 'uint32', 
+    setcode     : 'uint64', 
+    type        : 'uint32', 
+    level       : 'uint32', 
+    attribute   : 'uint32', 
+    race        : 'uint32', 
+    attack      : 'int32', 
+    defence     : 'int32'
+});
+
+
+var script_reader  = struct({
+    script_name :'char',
+    len         :'int',
 });
 var card_reader     = struct({
     code        :'uint32',
     data        :card_data,
 });
+var pduel = ref.refType('uint32');
 var message_handler = struct({
     pduel       : pduel,
     message_type: 'uint32'
 });
-
+var get_message_pointer = ref.refType('byte');
 var ocgapi = ffi.Library(__dirname + '/ocgcore.dll', {
-    'set_script_reader'  : ['void',  [script_reader] ],
-    'set_card_reader'    : ['void',  [card_reader] ],
-    'set_message_handler': ['void',  [message_handler] ],
+//    'set_script_reader'  : ['void',  [script_reader] ],
+//    'set_card_reader'    : ['void',  [card_reader] ],
+//    'set_message_handler': ['void',  [message_handler] ],
     'create_duel'        : ['pointer' ,  ['uint32'] ],
     'start_duel'         : ['void',  ['pointer', 'int'] ],
     'end_duel'           : ['void',  ['pointer']],
     'set_player_info'    : ['void',  ['pointer', 'int32', 'int32', 'int32', 'int32']],
     'get_log_message'    : ['void',  ['pointer', 'byte*']],
-    'get_message'        : ['int32', ['pointer', 'byte*']],
+    'get_message'        : ['int32', ['pointer', get_message_pointer]],
     'process'            : ['int32', ['pointer']],
     'new_card'           : ['void',  ['pointer', 'uint32', 'uint8', 'uint8', 'uint8', 'uint8', 'uint8']],
     'new_tag_card'       : ['void',  ['pointer', 'uint32', 'uint8', 'uint8']],
@@ -83,40 +98,78 @@ function_name(typeforParam1, typeforParam2, etc,..){
  dllprocessing...
  return type;
 }
-
 */
 console.log('OCGCore Engine Loaded');
+var player1deck = require('./sample.deck.json');
+var player2deck = require('./sample.deck.json');
+function loaddeck(deck, playerid){
+    for (var m = 0; m < deck.main.length; m++){
+        ocgapi.new_card(pduel, deck.main[m], playerid, playerid,0x2, m, 0x01);
+    }
+    for (var e = 0; e < deck.extra.length; e++){
+        ocgapi.new_card(pduel, deck.extra[e], playerid, playerid,0x40, e, 0x01);
+    }
+    
+}
 
 var mt = new MersenneTwister();
 var seed = mt.int();
 console.log('Virtualizing Duel :',seed);
 
 var pduel = ocgapi.create_duel(seed);
-
+//ocgapi.set_script_reader(script_reader);
+//ocgapi.set_card_reader(card_reader);
+//ocgapi.set_message_handler(message_handler);
 ocgapi.set_player_info(pduel, player1, configuration.startLP, configuration.starthand, configuration.draw);
-
 ocgapi.set_player_info(pduel, player2, configuration.startLP, configuration.starthand, configuration.draw);
-ocgapi.start_duel(pduel,1);
-function ask(){
-    var c= ocgapi.process(pduel);
- 
-    d = new Buffer(512);
-    ocgapi.get_message(pduel, d);
-    console.log(c);
-    out = d.readUInt32LE(0);
-    console.log(parseInt(out,10) );
-    
+loaddeck(player1deck,player1);
+loaddeck(player2deck,player2);
+ocgapi.start_duel(pduel,0);
+process();
+function process(){
+    var engFlag = 0, engLen = 0, stop = 0;
+    var engineBuffer = new Buffer(0x1000);
+    while (!stop){
+        if (engFlag == 2){ stop = 2; }
+        var result = ocgapi.process(pduel);
+        engLen = result & 0xffff;
+        engFlag = result >> 16;
+        
+
+        console.log('engLen :', engLen, '= 2 means everything is working');
+        console.log('engFlag :', engFlag);
+        if (engLen > 0) {
+            ocgapi.get_message(pduel, engineBuffer);
+            
+            stop = Analyze(engineBuffer, engLen);
+            
+            
+            
+        }
+    }
+    if(stop == 2){
+          
+    }
 }
-ask()
-ask()
-ask()
-ask()
-ask()
+function Analyze(msgbuffer, len){
+    
+    var  offset, pbufw, pbuf = msgbuffer;
+    var  player, count, type;
+    console.log(pbuf - msgbuffer );
+    while (pbuf - msgbuffer < len) {
+                console.log(pbuf - msgbuffer );
+                offset = pbuf;
+                var engType = Buffer.ReadUInt8(pbuf);
+        console.log('ana',engType);
+                        return 1;
+    }
+    return 0;
+
+}
+
 
 /* checks if we started from node(true), or started from the command prompt(false) */
-var launchpostion = (process.argv[0] === 'node') ?
-                    0 :
-                    2 ;
+
 
 
 
@@ -151,11 +204,11 @@ var banlist = (function(lflist){
 })(lflist);
 
 console.log('Listening at :', gamestate.url);
-var clientlistener = new WebSocket(gamestate.url);
-
-clientlistener.on('message', function(){
-    
-});
+//var clientlistener = new WebSocket(gamestate.url);
+//
+//clientlistener.on('message', function(){
+//    
+//});
 
 
 
